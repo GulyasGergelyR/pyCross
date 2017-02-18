c554e8b8ee9eeef59055c6da5530706d5459936f
Unknown <g.gergo42@gmail.com>
2017-02-18 16:01:58 +0100

    Element redesign

--- src/pyCross/base.py
+++ src/pyCross/base.py
@@ -3,81 +3,165 @@ import numpy as np
 # Default Size
 DEF_SIZE = 10
 # First ID
-current_id = 10
+element_id = 10
+
+dim_x_id = 1000
+x_id = 10
+dim_y_id = 2000
+y_id = 10
+dim_z_id = 3000
+z_id = 10
 
 
-def get_next_id():
-    global current_id
-    current_id += 1
-    return current_id
+def get_id():
+    global element_id
+    element_id += 1
+    return element_id
 
 
-class Base(object):
+class Cell(object):
     def __init__(self, **kwargs):
-        self.color = kwargs.pop('color', -1)
-        self.id = kwargs.pop('id', -1)
-
-
-class Cell(Base):
-    def __init__(self, x, y, **kwargs):
-        super(Cell, self).__init__(**kwargs)
-        self.x = x
-        self.y = y
+        self.p = kwargs.pop('p', [0, 0, 0])
+        self.id = kwargs.pop('id', [-1, -1, -1])
+        self.color = -1
 
 
 class Column(object):
     def __init__(self, **kwargs):
-        self.length = kwargs.pop('length', DEF_SIZE)
         self.cells = kwargs.pop('cells', [])
+        self.length = len(self.cells)
+        self.vector = kwargs.pop('vector', None)
+        if self.vector is not None:
+            self.vector.column = self
+
+        self.parent = kwargs.pop('parent', None)
+
+        self.left_clone = Column(parent=self)
+        self.right_clone = Column(parent=self)
 
 
-class Table2D(object):
+class Element(object):
     def __init__(self, **kwargs):
-        self.size = kwargs.pop('size', DEF_SIZE)
-        self.cells = np.array([[Cell(j, i) for j in range(self.size)] for i in range(self.size)])
+        """
 
-    def get(self, x=None, y=None):
-        if x is not None and y is not None:
-            return self.cells[y][x]
-        if x is not None:
-            return self.cells[:][x]
-        if y is not None:
-            return self.cells[y][:]
-        raise ValueError('Did not specify what to get from column!')
+        :param kwargs:
+         vector: Vector element which stores the elements of a specified column
+         id: Unique id for an element
+         color: color id
+         length: length of the element
+         pos: position in the column
+         found: boolean - If this is true, then this element cannot be moved
+         tc: test column
+        """
+        self.vector = kwargs.pop('vector', None)
 
-
-class Element(Base):
-    def __init__(self, **kwargs):
-        kwargs['id'] = kwargs.pop('id', get_next_id())
-        super(Element, self).__init__(**kwargs)
+        self.id = kwargs.pop('id', get_id())
+        self.color = kwargs.pop('color', 1)
         self.length = kwargs.pop('length', 1)
+
+        self.pos = kwargs.pop('pos', None)
+        self.found = kwargs.pop('found', False)
+
+        self.tc = kwargs.pop('tc', [-1 for _ in range(self.column)])
+
+    @property
+    def end(self):
+        return self.pos + self.length - 1
+
+    @property
+    def column(self):
+        return self.vector.column
+
+    @property
+    def cells(self):
+        return self.column.cells
+
+    @property
+    def colors(self):
+        return [cell.color for cell in self.cells]
+
+    @property
+    def ids(self):
+        return [cell.color for cell in self.cells]
+
+    def spacer(self, element):
+        if self.color == element.color:
+            return 1
+        else:
+            return 0
+
+    def get_left(self):
+        return self.vector.get(self, -1)
+
+    def get_right(self):
+        return self.vector.get(self, +1)
+
+    def set_invalid(self, i=0, j=None, length=0):
+        j = j if j is not None else i+length
+        for k in range(i, j):
+            self.tc[k] = 0
+
+    def find_valid_pos(self, anchor=None):
+        # Find invalid positions
+        left = self.get_left()
+        if left is None:
+            left_pos = 0
+        else:
+            left_pos = left.end + self.spacer(left)
+        # left_pos is the first valid cell after the prev element (without main constraints)
+        self.set_invalid(j=left_pos)
+        # left: [XXXXXXX111-----]
+        # self: [XXXXXXXXXXX11--] length: 2, color: 1
+        for i, c in enumerate(self.colors[left_pos:]):
+                self.tc[i] = c
+
+            # main: [--2-1XX--333---]
+            # left: [X22------------] length: 2, color: 2
+            #           |----------|  iterate here
+            # self: [XXX-1XX--333---] color: 1
+
+        # Find valid positions
+        valid = False
+        if anchor is not None:
+            # anchor is the end point
+            pos = anchor - self.length + 1
+        else:
+            pos = left_pos
+        right = self.get_right()
+        pattern = [self.color for _ in range(self.length)]
+        if right is not None:
+            if self.color == right.color:
+                pattern += [0]
+        while not valid:
+            for i, c in enumerate(pattern):
+                if self.tc[pos + i] != -1 and self.tc[pos + i] != c:
+                    # we failed at the ith element
+                    # we start again at pos + i + 1
+                    pos += i + 1
+                    # set prev points to invalid
+                    self.set_invalid(j=pos)
+                    break
+            else:
+                valid = True
+
+        if valid:
+            self.pos = pos
 
 
 class Vector(object):
     def __init__(self, **kwargs):
         self.elements = kwargs.pop('elements', [])
+        self.column = None
 
     def add(self, element):
         self.elements += [element]
+        element.vector = self
 
-    def get(self, element):
-        for e in self.elements:
+    def get(self, element, offset=0):
+        for i, e in enumerate(self.elements):
             if element.id == e.id:
-                return self.elements
-        return None
-
-
-class Map2D(object):
-    def __init__(self, **kwargs):
-        self.size = kwargs.pop('size', DEF_SIZE)
-        self.X = [Vector() for _ in self.size]
-        self.Y = [Vector() for _ in self.size]
-
-        self.table_2d = None
-
-    def assign_table_2d(self, table_2d):
-        self.table_2d = table_2d
-        self._parse_table()
-
-    def _parse_table(self):
-        pass
+                if len(self.elements) > i+offset >= 0:
+                    return e[i+offset]
+                else:
+                    return None
+        raise ValueError("There is no such element with id: '{}'".format(element.id))
